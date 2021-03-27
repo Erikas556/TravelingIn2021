070e5f336a71efc9b19af90b581b2825beae2e31
ViliusU <vilius.utaravicius@gmail.com>
2021-03-27 13:22:47 +0200

    Fixed error

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -5,12 +5,19 @@ import org.junit.jupiter.api.Test;
 
 class TestJunit {
 
-@Test
-    public void testingJunit(){
+    @Test
+    public void testAssertEquals() {
+        //given
+        int firstNum = 5;
+        int secondNum = 20;
 
-}
+        //when
+        int result = firstNum * secondNum;
 
-
+        //then
+        Assertions.assertEquals(100, result);
+        Assertions.assertNotEquals(50, result);
+    }
 
 
 }
