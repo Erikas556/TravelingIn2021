b071ece3c2077a195995a5e91324d0433b7cb719
ViliusU <vilius.utaravicius@gmail.com>
2021-03-28 14:11:41 +0300

    First try testing: created CountryInfo class Country Manager class

---
+++ src/main/java/entity/CountryInfo.java
@@ -0,0 +1,70 @@
+package entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "countryInfo")
+public class CountryInfo {
+    private int id;
+    private String countryName;
+    private boolean vaccineRequirement;
+    private boolean testRequirement;
+    private int testValidationTimeByHours;
+    private boolean IsForeigner;
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public boolean isVaccineRequirement() {
+        return vaccineRequirement;
+    }
+
+    public void setVaccineRequirement(boolean vaccineRequirement) {
+        this.vaccineRequirement = vaccineRequirement;
+    }
+
+    public boolean isTestRequirement() {
+        return testRequirement;
+    }
+
+    public void setTestRequirement(boolean testRequirement) {
+        this.testRequirement = testRequirement;
+    }
+
+    public int getTestValidationTimeByHours() {
+        return testValidationTimeByHours;
+    }
+
+    public void setTestValidationTimeByHours(int testValidationTimeByHours) {
+        this.testValidationTimeByHours = testValidationTimeByHours;
+    }
+
+    public boolean isForeigner() {
+        return IsForeigner;
+    }
+
+    public void setForeigner(boolean foreigner) {
+        IsForeigner = foreigner;
+    }
+}
---
+++ src/main/java/sessionFactory/CountryManager.java
@@ -0,0 +1,64 @@
+package sessionFactory;
+
+import entity.CountryInfo;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class CountryManager {
+    private SessionFactory sessionFactory;
+
+    private void setup() {
+        // code to load Hibernate Session factory
+
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure() // configures settings from hibernate.cfg.xml
+                .build();
+        try {
+            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+        } catch (Exception ex) {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+
+    }
+
+    private void exit() {
+        // code to close Hibernate Session factory
+    }
+
+    private void create() {
+        // code to save
+
+    }
+
+    private void read() {
+        // code to get
+        Session session = sessionFactory.openSession();
+
+        int ID = 0;
+        CountryInfo countryInfo = session.get(CountryInfo.class, ID);
+
+        System.out.println("Country Name: " + countryInfo.getCountryName());
+        System.out.println("Vaccine Requirement: " + countryInfo.isVaccineRequirement());
+        System.out.println("Test Requirement: " + countryInfo.isTestRequirement());
+        System.out.println("Test Validation Time By hours: " + countryInfo.getTestValidationTimeByHours());
+
+       session.close();
+    }
+
+    private void update() {
+        // code to modify
+    }
+
+    private void delete() {
+        // code to remove
+    }
+
+    public static void main(String[] args) {
+        // code to run the program
+        CountryManager manager = new CountryManager();
+        manager.read();
+    }
+}
