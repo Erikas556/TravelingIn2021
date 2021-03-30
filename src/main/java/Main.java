c06fdb15e1278d3fb190c79cf3a508c7eb4ff795
ViliusU <vilius.utaravicius@gmail.com>
2021-03-30 23:39:17 +0300

    Made changes to start application from Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello Traveling In 2021");
+        System.out.println("===== Hello Traveling In 2021 =====");
         CountryManager.main();
 
     }
